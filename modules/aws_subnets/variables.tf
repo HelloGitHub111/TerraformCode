e65899badb975a204910e7bcd0b4f412317aa8c2
HelloGitHub111 <35871743+HelloGitHub111@users.noreply.github.com>
2023-10-07 19:47:04 -0500

    Add files via upload

---
+++ modules/aws_subnets/variables.tf
@@ -0,0 +1,20 @@
+variable "cidr_block" {
+
+    default = "10.0.1.0/24"
+  
+}
+
+variable "tags" {
+  default = {
+    Name = "Main"
+  }
+}
+
+variable "availability_zone" {
+  
+}
+
+
+variable "vpc_id" {
+  
+}
